470e408a00862ba4bea17f15092dcf2f1c70f670
linuxdream <bdching@gmail.com>
2017-09-23 10:51:56 -0700

    Move everything to promises.
    Now saves data to Mongo.

--- saveTumblr.js
+++ saveTumblr.js
@@ -1,7 +1,23 @@
-const config = require( 'dotenv' ).config();
+require( 'dotenv' ).config();
 const _ = require( 'lodash' );
 const Inquirer = require( 'inquirer' );
 const Tumblr = require( 'tumblr.js' );
+const Mongoose = require( 'mongoose' );
+
+const Schema = Mongoose.Schema;
+
+// Connect to Mongo
+Mongoose.connect( 'mongodb://localhost/tumblr' );
+const tumblrSchema = new Schema( {
+    'tumblrID': Number,
+    'postUrl': String,
+    'postDate': String,
+    'caption': String,
+    'photos': Schema.Types.Mixed,
+    'createdAt': Date
+} );
+
+let tumblrDB = Mongoose.model( 'posts', tumblrSchema );
 
 /**
  * Connect to Tumblr
@@ -30,38 +46,104 @@ getBlogs().then( ( blogs ) => {
     /**
      * How to backup question
      */
-    let backupTypeQuestion = {
-        'type': 'list',
-        'name': 'backupType',
-        'message': 'How would you like to save the images?',
-        'choices': [
-            'images/',
-            'images/YYYY-MM-DD',
-            'images/YYYY/MM/DD'
-        ]
-    };
+    // let backupTypeQuestion = {
+    //     'type': 'list',
+    //     'name': 'backupType',
+    //     'message': 'How would you like to save the images?',
+    //     'choices': [
+    //         'images/',
+    //         'images/YYYY-MM-DD',
+    //         'images/YYYY/MM/DD'
+    //     ]
+    // };
 
-    inquireQuestions.push( backupTypeQuestion );
+    // inquireQuestions.push( backupTypeQuestion );
 
     Inquirer.prompt( inquireQuestions ).then( ( answers ) => {
+        /**
+         * Initial request 
+         */
         tumblrClient.blogPosts( answers.blogName, {'type': 'photo'}, ( err, resp ) => {
+            if ( err ) {
+                throw new Error( err );
+            }
+
             let totalPosts = resp.total_posts;
 
             /**
-             * Tumblr only allows a max of 20 posts so if they hav eless than that
+             * Tumblr only allows a max of 20 posts so if they have less than that
              * just save and exit.
              */
             if ( totalPosts > 20 ) {
                 /**
                  * Save the first response
                  */
-                savePosts( resp.posts );
+                let requestArray = [];
+
+                for ( let i = 0; i < totalPosts; i += 20 ) {
+                    requestArray.push( getPosts( answers.blogName, i ) );
+                }
+
+                Promise.all( requestArray ).then( ( responses ) => {
+                    let savePromises = [];
+
+                    responses.forEach( ( posts ) => {
+                        savePromises.push( savePosts( posts ) );
+                    } );
+
+                    Promise.all( savePromises ).then( ( values ) => {
+                        console.log( 'All done' );
+                        process.exit();
+                    } );
+                } );
             } else {
                 savePosts( resp.posts );
             }
         } );
     } );
 } );
+
+function getPosts ( blogName, offset = 0 ) {
+    return new Promise( ( resolve, reject ) => {
+        tumblrClient.blogPosts( blogName, {'type': 'photo', 'offset': offset}, ( err, resp ) => {
+            if ( err ) {
+                return reject( err );
+            }
+
+            return resolve( resp.posts );
+        } );
+    } );
+}
+
+/**
+ * Save the post data locally
+ */
+function savePosts ( posts ) {
+    return new Promise( ( resolve, reject ) => {
+        if ( !Array.isArray( posts ) ) {
+            return reject( new Error( 'Array not passed' ) );
+        }
+
+        let insertPromises = [];
+
+        posts.forEach( ( post ) => {
+            let newRecord = {
+                'tumblrID': post.id,
+                'postUrl': post.post_url,
+                'postDate': post.date,
+                'caption': post.caption,
+                'photos': post.photos,
+                'createdAt': new Date()
+            };
+
+            insertPromises.push( tumblrDB.create( newRecord ) );
+        } );
+
+        Promise.all( insertPromises ).then( ( values ) => {
+            return resolve( );
+        } );
+    } );
+}
 
 /**
  * Get a list of their blogs
